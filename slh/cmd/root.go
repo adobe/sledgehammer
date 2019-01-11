69b8c3d195a0aa5e831a805ff228fc608122c69c
Nils Plaschke <plaschke@adobe.com>
2019-01-11 13:01:47 +0100

    Added none output type

--- slh/cmd/root.go
+++ slh/cmd/root.go
@@ -12,6 +12,8 @@ written permission of Adobe.
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -35,7 +37,11 @@ Use dockerized tools like they are installed on the  system.`,
 
 	var logLevel string
 	rootCommand.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
-		initializeLogger(cfg, logLevel, cfg.OutputType == "json")
+		if err := validateOutputType(cfg.OutputType); err != nil {
+			return err
+		}
+
+		initializeLogger(cfg, logLevel, cfg.OutputType)
 
 		if err := configureOutput(cfg); err != nil {
 			return err
@@ -58,7 +64,7 @@ Use dockerized tools like they are installed on the  system.`,
 		return nil
 	}
 
-	rootCommand.PersistentFlags().StringVarP(&cfg.OutputType, "output", "o", "text", "Define the output, currently supported is table|json")
+	rootCommand.PersistentFlags().StringVarP(&cfg.OutputType, "output", "o", "text", "Define the output, currently supported is none|text|json")
 	rootCommand.PersistentFlags().StringVar(&logLevel, "log-level", "none", "Set the log level (debug|info|warning|error|fatal|panic)")
 	rootCommand.PersistentFlags().StringVar(&cfg.ConfigDir, "confdir", cfg.ConfigDir, "Location of configuration directory. Default is bindir/.slh")
 
@@ -103,7 +109,21 @@ func configureOutput(cfg *config.Config) error {
 	return nil
 }
 
-func initializeLogger(cfg *config.Config, logLevel string, JSON bool) {
+func validateOutputType(output string) error {
+	validOutputs := []string{"none", "text", "json"}
+	match := false
+	for _, v := range validOutputs {
+		if v == output {
+			match = true
+		}
+	}
+	if !match {
+		return fmt.Errorf("The output type %s does not match %v", output, validOutputs)
+	}
+	return nil
+}
+
+func initializeLogger(cfg *config.Config, logLevel string, output string) {
 	//  init logrus
 	logrus.SetOutput(cfg.IO.Out)
 
@@ -113,8 +133,11 @@ func initializeLogger(cfg *config.Config, logLevel string, JSON bool) {
 	} else {
 		logrus.SetLevel(level)
 	}
-	if JSON {
+	if output == "json" {
 		logrus.SetFormatter(&logrus.JSONFormatter{})
+	}
+	if output == "none" {
+		logrus.SetOutput(ioutil.Discard)
 	}
 }
 
@@ -131,7 +154,7 @@ func setConfigDir(cfg *config.Config) error {
 
 func output(cfg *config.Config) error {
 	// render the output now
-	if cfg.Output != nil {
+	if cfg.Output != nil && cfg.OutputType != "none" {
 		if cfg.Output.Element != nil {
 			cfg.Output.Render()
 		} else {
